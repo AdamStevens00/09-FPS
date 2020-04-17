b162aaab0ba04af259c26530ed0de7ce52e383a6
AdamStevens00 <59852039+AdamStevens00@users.noreply.github.com>
2020-04-16 23:09:36 -0400

    this is my unfinished project 9

---
+++ Project-Template-Godot-master/Scenes/Global.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+var Score = get_node("/root/Game/HUD/Score")
+
+
+func _ready():
+	pass 
+
+
--- Project-Template-Godot-master/Scenes/Scripts/Player.gd
+++ Project-Template-Godot-master/Scenes/Scripts/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 onready var Camera = $Pivot/Camera
+var bullet = preload("res://scenes/bullet.tscn")
 
 var velocity = Vector3()
 var gravity = -9.8
@@ -11,7 +12,7 @@ var jump = 10
 var jumping = false
 
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func get_input():
 	var input_dir = Vector3()
@@ -42,8 +43,12 @@ func _physics_process(delta):
 	velocity = move_and_slide(velocity, Vector3.UP, true)
 
 func _unhandled_input(event):
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Pivot.rotation.x = clamp($Pivot.rotation.x, -mouse_range, mouse_range)
-		
+	if event.is_action_pressed("shoot") and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		var b = bullet.instance()
+		b.start($Pivot/Muzzle.global_transform)
+		get_node("/root/Game/Bullets").add_child(b)
+
